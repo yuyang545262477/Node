f5c2cbf24457e43aea5891b634a997b19cb2b668
David_Yu <yuyang545262477@gmail.com>
2016-04-10 18:03:24 +0800

    再试一次.

---
+++ BlogNode/app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+
+//路由
+var routes = require('./routes/index');
+
+var app = express();
+
+// app.set('port', process.env.PORT || 3000);//设置端口
+app.set('views', path.join(__dirname, 'views'));//设置视图
+app.set('view engine', 'ejs');//设置视图模板
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+routes(app);
+console.log("测试 是否....");
+app.listen(app.get('port'), function () {
+    console.log("Express server listening on port", app.get('port'));
+});
+
+module.exports = app;
---
+++ BlogNode/routes/index.js
@@ -0,0 +1,35 @@
+module.exports = function (app) {
+    //首页
+    app.get('/', function (req, res) {
+        res.render('index', {title: '主页'});
+    });
+
+
+    //注册页面
+    app.get('/reg', function (req, res) {
+        res.render('reg', {title: '注册'});
+    });
+    app.post('/reg', function (req, res) {
+
+    });
+    //登录页面
+    app.get('/login', function (req, res) {
+        res.render('reg', {title: '登录'});
+    });
+    app.post('/login', function (req, res) {
+
+    });
+    //博文
+    app.get('/post', function (req, res) {
+        res.render("post", {title: '发表'})
+    });
+    app.post('/post', function (req, res) {
+
+    });
+
+//    登出.
+    app.get('/logout', function (req, res) {
+
+    });
+
+};
